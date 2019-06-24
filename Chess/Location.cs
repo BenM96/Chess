279cc8a1d68a6cc8e63af5c1032c6c578b3f6d4a
BenM96 <1996bmercer@gmail.com>
2019-06-24 15:27:47 +0100

    first commit

---
+++ Chess/Location.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Location
+    {
+        private char row;
+        private int col;
+        private string Alphabet = "0ABCDEFGH";
+        public char Row
+        {
+            get
+            {
+                return this.row;
+            }
+            set
+            {
+                this.col = value;
+            }
+        }
+
+        
+    }
+}
---
+++ Chess/Piece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    abstract class Piece
+    {
+        private Location loaction;
+
+        public abstract bool move(Location too);
+
+    }
+}
